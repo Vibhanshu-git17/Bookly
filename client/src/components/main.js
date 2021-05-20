bbdff8640541c65c790ea85313dc4b389a1ce676
Vibhanshu chaubey <51204314+thdjoker@users.noreply.github.com>
2021-05-20 13:53:12 +0530

    update code 1

--- client/src/components/main.js
+++ client/src/components/main.js
@@ -7,7 +7,7 @@ export default props => {
       <Row noGutters className="text-center align-items-center pizza-cta">
         <Col>
           <p className="looking-for-pizza">
-            If you're looking for great Cuisine
+            If you're looking for Cuisine
             <i className="fas fa-pizza-slice pizza-slice"></i>
           </p>
           <Button
